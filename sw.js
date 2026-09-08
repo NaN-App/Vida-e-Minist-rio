b44d223e03948664eab943cfbcdb7e1c5bd24f06
NaN-App <renanmoreira89@gmail.com>
2026-09-08 08:31:18 -0300

    Update CACHE_NAME to version 1.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cronometro-v1.2';
+const CACHE_NAME = 'cronometro-v1.3';
 const ASSETS = [
   './',
   './index.html', // Caso seu arquivo principal se chame index.html no GitHub
